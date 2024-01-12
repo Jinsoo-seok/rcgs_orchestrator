a0b1fa74b821761b066d58102108e5d5c09b23e9
SJS <jinsoo1932@gmail.com>
2024-01-12 16:15:20 +0900

    Initial commit

---
+++ src/main/java/com/cudo/rcgs_orchestrator/RcgsOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.cudo.rcgs_orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcgsOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RcgsOrchestratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cudo/rcgs_orchestrator/RcgsOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cudo.rcgs_orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcgsOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
